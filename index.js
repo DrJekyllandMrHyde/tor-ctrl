62ffff2f7ead7796e7b37334fd947dabfff373aa
DrJekyllandMrHyde <brewbaker@ya.ru>
2017-06-21 08:42:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const minify = require('./lib/minify');
 const controllers = require('./controllers');
 
 const app = express();
-const port = process.env.port || 443;
+const port = process.env.port || 80;
 
 // Trust proxy headers if we're deployed on now
 if (process.env.NOW) {
